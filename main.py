98e2f3a4f6e0e48f29aed5666f5175c04a04c0c3
Артём Мирумян <armirumian@edu.hse.ru>
2025-03-16 21:24:46 +0300

    merged from main

--- main.py
+++ main.py
@@ -6,5 +6,8 @@ app = FastAPI(title="Movie Recommendation Backend")
 app.include_router(router)
 
 
+#########################################
+# Запуск сервера через uvicorn          #
+#########################################
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8005, reload=True)
